7ff484f2532986cd7610854dccab13937426fb65
kdkdirjrne <ruigie2007gabotero@gmail.com>
2026-06-29 10:56:53 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/eac3e854828e7370eb376b294516be51c3deb4991f5d796f8eb80d8a696be4cc/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3bcc0a2b7346a8bfe88b2d431526b03afd2001118730792679c2a564be0e40d1/download"))()
