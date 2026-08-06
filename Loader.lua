6e30dab2e259ad89fa7d892abeb0f0da4a387f05
kdkdirjrne <ruigie2007gabotero@gmail.com>
2026-08-07 03:07:40 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3bcc0a2b7346a8bfe88b2d431526b03afd2001118730792679c2a564be0e40d1/download"))()
+-- tryna skid is simply buy it it cheap dont be gay
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kdkdirjrne/Vgxmod-Hub/refs/heads/main/Load3r.lua"))()
